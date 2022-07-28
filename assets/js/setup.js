68e30d8681096a6757cdfbb4bcee5de3e0708a29
Stockriderj <jason.qian.36@gmail.com>
2022-07-28 18:56:13 -0500

    Added responsive favicon

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,3 +1,13 @@
+// Head
+document.querySelector("head").innerHTML += `
+
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/favicon/site.webmanifest">
+
+`;
+
 // Navbar
 const navbarEl = $("#main-header nav");
 const navbar = `
