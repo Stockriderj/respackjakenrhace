2c6baf2b24080034be37ed03ef778946190aaa20
Stockriderj <jason.qian.36@gmail.com>
2022-07-28 13:15:26 -0500

    Added styles

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,10 +1,14 @@
+// Navbar
 const navbarEl = $("#main-header nav");
 const navbar = `
 
 <div class="container-fluid px-5">
-    <a class="navbar-brand" href="index.html"><img class="rounded-3 me-2" src="assets/images/respak-flag.jpeg" alt="Respackjakenrhace" width="50"><span>Respackjakenrhace</span></a>
+    <a class="navbar-brand" href="index.html">
+        <img class="rounded-3 me-2" src="assets/images/respak-flag.jpeg" alt="Respackjakenrhace" width="10%">
+        <span>Respackjakenrhace</span>
+    </a>
 </div>
 
 `;
-navbarEl.addClass("navbar bg-light");
+navbarEl.addClass("navbar bg-primary navbar-dark");
 navbarEl.html(navbar);
