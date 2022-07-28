01c98a30fefb333eab89ef225e4d8683aa5c1455
Stockriderj <jason.qian.36@gmail.com>
2022-07-28 14:50:55 -0500

    Added language.html

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -7,6 +7,13 @@ const navbar = `
         <img class="rounded-3 me-2" src="assets/images/respak-flag.jpeg" alt="Respackjakenrhace" width="10%">
         <span>Respackjakenrhace</span>
     </a>
+    <ul class="navbar-nav">
+    
+        <li class="navbar-item">
+            <a class="nav-link" href="language.html">Language</a>
+        </li>
+        
+    </ul>
 </div>
 
 `;
