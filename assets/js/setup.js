020a09882127a178c9b0d35c3fc275b0d425cb32
Stockriderj <jason.qian.36@gmail.com>
2022-07-29 15:40:56 -0500

    Added origin.html and more setup versatility

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -11,32 +11,32 @@ function getImageFile(filename) {
 
 // Head
 document.querySelector("head").innerHTML += `
-
     <link rel="apple-touch-icon" sizes="180x180" href="${getImageFile("favicon")}">
     <link rel="icon" type="image/png" sizes="32x32" href="${getImageFile("favicon/favicon-32x32.png")}">
     <link rel="icon" type="image/png" sizes="16x16" href="${getImageFile("favicon/favicon-16.16")}">
     <link rel="manifest" href="${getImageFile("favicon/site.webmanifest")}">
-
 `;
 
 // Navbar
 const navbarEl = $("#main-header nav");
 const navbar = `
-
 <div class="container-fluid px-5">
+
     <a class="navbar-brand" href="index.html">
-        <img class="rounded-3 me-2" src="assets/images/respak-flag.jpeg" alt="Respackjakenrhace" width="10%">
+        <img class="rounded-3 me-2" src="${getImageFile("respak-flag.jpeg")}" alt="Respackjakenrhace" width="10%">
         <span>Respackjakenrhace</span>
     </a>
-    <ul class="navbar-nav">
-    
-        <li class="navbar-item">
-            <a class="nav-link" href="language.html">Language</a>
-        </li>
-        
-    </ul>
-</div>
 
+    <div class="navbar-nav">
+        <a class="nav-link" href="origin.html">Origin</a>
+        <a class="nav-link" href="language.html">Language</a>
+    </div>
+
+</div>
 `;
-navbarEl.addClass("navbar bg-primary navbar-dark");
+navbarEl.addClass("navbar navbar-expand-sm bg-primary navbar-dark");
 navbarEl.html(navbar);
+
+// Main
+const mainEl = $("main");
+mainEl.addClass("container my-3");
